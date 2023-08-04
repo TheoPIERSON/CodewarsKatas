2b2f1ccef73e6499436f5b7a8e0848caf7a54e87
Theo Pierson <thheopierson@gmail.com>
2023-08-04 11:34:33 +0200

    add: new java exercices

---
+++ JAVA/01-multiply.java
@@ -0,0 +1,15 @@
+//Multiply
+
+/*This code does not execute properly. Try to figure out why. */
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b
+    }
+}
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
